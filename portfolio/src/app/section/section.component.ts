ee41347d07a5c00cca8bb90f243d0dde2487b16c
Rona Elias <ronaelias1@gmail.com>
2024-07-01 23:13:30 +0300

    section changed

---
+++ portfolio/src/app/section/section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './section.component.html',
+  styleUrl: './section.component.scss'
+})
+export class SectionComponent {
+
+}
