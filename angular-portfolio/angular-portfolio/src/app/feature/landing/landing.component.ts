a4112f719eb303b199d557d57682c52241876dc5
Pascaline <dagherpascaline@gmail.com>
2024-07-03 01:25:12 +0300

    feat: add about model

--- angular-portfolio/angular-portfolio/src/app/feature/landing/landing.component.ts
+++ angular-portfolio/angular-portfolio/src/app/feature/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AboutDetail } from '../../models/about-detail.model';
 
 @Component({
   selector: 'app-landing',
@@ -6,6 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+  aboutDetails: AboutDetail[] = [
+    {
+      icon: './assets/experience.png',
+      title: 'Experience',
+      description: '3+ years<br/>Frontend Development'
+    },
+    {
+      icon: './assets/education.png',
+      title: 'Education',
+      description: 'B.Sc. Bachelors Degree<br/>Lebanese University'
+    }
+  ];
+  
   openUrl(url: string): void {
     window.open(url, '_blank');
   }
---
+++ angular-portfolio/angular-portfolio/src/app/models/about-detail.model.ts
@@ -0,0 +1,5 @@
+export interface AboutDetail {
+    icon: string;
+    title: string;
+    description: string;
+  }  
