e839c06fb4b4206a1e6ae861de3c0fb2bc46f3e6
Pascaline <dagherpascaline@gmail.com>
2024-07-02 23:45:44 +0300

    feat: add about component in feature module

--- angular-portfolio/angular-portfolio/src/app/feature/feature.module.ts
+++ angular-portfolio/angular-portfolio/src/app/feature/feature.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    LandingComponent
+    LandingComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LandingComponent
+    LandingComponent,
+    AboutComponent
   ]
 })
 export class FeatureModule { }
