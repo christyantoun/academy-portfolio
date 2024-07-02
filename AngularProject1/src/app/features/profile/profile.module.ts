2f528ffaa4d50409228c09b3764b247d3bcfd57f
JoeyA357 <135324158+JoeyA357@users.noreply.github.com>
2024-07-03 01:38:25 +0300

    Project Section Complete

--- AngularProject1/src/app/features/profile/profile.module.ts
+++ AngularProject1/src/app/features/profile/profile.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProfileSummaryComponent } from './profile-summary/profile-summary.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
 @NgModule({
-  declarations: [ProfileSummaryComponent, AboutMeComponent],
+  declarations: [ProfileSummaryComponent, AboutMeComponent, ProjectsComponent],
   imports: [
     CommonModule,
     FormsModule
   ],
-  exports: [ProfileSummaryComponent, AboutMeComponent]
+  exports: [ProfileSummaryComponent, AboutMeComponent, ProjectsComponent], 
 })
 export class ProfileModule { }
