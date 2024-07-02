5b37ca64ddd13d97ff43783aa5e6da5ac489be15
Pascaline <dagherpascaline@gmail.com>
2024-07-03 00:20:02 +0300

    feat: add project component in feature module

--- angular-portfolio/angular-portfolio/src/app/feature/feature.module.ts
+++ angular-portfolio/angular-portfolio/src/app/feature/feature.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
 
 
 
 @NgModule({
   declarations: [
     LandingComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     LandingComponent,
-    AboutComponent
+    AboutComponent,
+    ProjectComponent
   ]
 })
 export class FeatureModule { }
