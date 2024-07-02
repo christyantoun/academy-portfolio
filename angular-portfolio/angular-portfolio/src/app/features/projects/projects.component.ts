9a217985b76d32d1b7eafe4512c37ce84e9b6af1
BrunoFares <faresbruno04@gmail.com>
2024-07-03 02:01:55 +0300

    Added a confirm system to the buttons of the project component.

--- angular-portfolio/angular-portfolio/src/app/features/projects/projects.component.ts
+++ angular-portfolio/angular-portfolio/src/app/features/projects/projects.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  onClick(link: string, type: string) {
+    if (type == 'demo') {
+      if (confirm("Do you want to see a Live Demo of the project?")) {
+        location.href = link
+      }
+    }
+    else if (type == 'github') {
+      if (confirm("Do you want to see the project's GitHub page?")) {
+        location.href = link
+      }
+    }
+  }
 }
