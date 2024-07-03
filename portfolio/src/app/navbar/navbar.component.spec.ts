4c698b029e77ec7094e42ba78c156c4384057747
Rona Elias <ronaelias1@gmail.com>
2024-07-03 10:29:55 +0300

    navbar component added

--- portfolio/src/app/navbar/navbar.component.spec.ts
+++ portfolio/src/app/navbar/navbar.component.spec.ts
@@ -8,7 +8,7 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      declarations: [NavbarComponent]
     })
     .compileComponents();
 
--- portfolio/src/app/navbar/navbar.component.ts
+++ portfolio/src/app/navbar/navbar.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
-  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
