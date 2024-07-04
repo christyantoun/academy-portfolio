715aa226458f88959991473e0dab77a4ee3c6bfa
Rona Elias <ronaelias1@gmail.com>
2024-07-04 14:47:53 +0300

    now done

--- myportfolio/src/app/features/about/about.model.ts
+++ myportfolio/src/app/features/about/about.model.ts
@@ -4,6 +4,6 @@ export interface About{
     imageUrl: string;
     cvUrl: string;
     contactUrl: string;
-    liUrl: string;
-    ghUrl: string;
+    linkedInUrl: string;
+    gitHubUrl: string;
 }
