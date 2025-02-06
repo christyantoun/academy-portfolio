4cd6c1a4d057ddc51f735637ab71b664b85e5cb8
carmen-merheb <carmen.merheb@lau.edu>
2025-02-06 13:01:07 +0200

    Initialization of navbar component

---
+++ Desktop/academy-portfolio-main/class-work/session-one/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { RouterOutlet } from '@angular/router'; 
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NavbarComponent, RouterOutlet],
+  template: `<app-navbar></app-navbar> <router-outlet></router-outlet>`,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {}
