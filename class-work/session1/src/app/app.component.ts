6026d98daad7b86cd162ca06c5700395ef99f298
Hani <hani.z.abdelghani@net.usek.edu.lb>
2025-02-06 05:43:35 +0200

    Components Imports

--- class-work/session1/src/app/app.component.ts
+++ class-work/session1/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component'
 import { HeroComponent } from './features/components/hero/hero.component'
+import { AboutMeComponent } from './features/components/about-me/about-me.component'
+import { MyProjectsComponent } from './features/components/my-projects/my-projects.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, AboutMeComponent, MyProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
